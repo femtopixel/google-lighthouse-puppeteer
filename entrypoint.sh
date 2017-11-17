535e8d4ebe44b4e5878ebce3599c80030cd4db25
Jay MOULIN <jaymoulin@gmail.com>
2017-11-17 10:56:58 +0100

    allow to pass parameters directly to container

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,10 @@
 set -e
 
 if [[ -f "/home/chrome/testcases/$1.js" ]]; then
-    lighthouse-puppeteer "/home/chrome/testcases/$1.js" "$2"
-else
-    exec "$@"
+    set -- lighthouse-puppeteer "/home/chrome/testcases/$1.js" "${@:2}"
+elif [ "${1#-}" != "$1" ]; then
+    set -- lighthouse-puppeteer "$@"
 fi
+
+exec "$@"
 
