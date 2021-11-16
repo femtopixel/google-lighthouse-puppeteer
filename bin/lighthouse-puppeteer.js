8c011e9a7381dc0568464fb79a13bbb5109a63da
Victor Heng <victor_heng@mom.gov.sg>
2021-11-16 19:29:33 +0800

    docs: Improve description of verbose functionality.

--- bin/lighthouse-puppeteer.js
+++ bin/lighthouse-puppeteer.js
@@ -55,7 +55,7 @@ const optionDefinitions = [
     {
         name: 'verbose',
         alias: 'v',
-        description: 'The more you add, the more it show information',
+        description: 'Providing this option once will show more info from Lighthouse. Providing it twice will additionally provide info from `lighthouse-batch`.',
         type: Boolean,
         multiple: true,
         group: 'main',
